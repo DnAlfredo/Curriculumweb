22ebe64f02e70c98ba6e061d343cfa7edb5cbb1f
DnAlfredo <alfredmp6@hotmail.com>
2022-10-15 13:40:17 +0200

    No encuentro combi colores, rumio js

--- jscurriculumweb.js
+++ jscurriculumweb.js
@@ -1,15 +1,16 @@
 function muestraDiv(idloca) {
-   
-     let divgoster = document.getElementById(idloca)
-    divgoster.classList.toggle('mostrar')
+
+    let divgoster = document.getElementById(idloca)
+    divgoster.classList.toggle('mostrar') 
     divgoster.classList.toggle('esconder')
+
+    document.getElementsByClassName('mostrar').style="esconder"
    
-    
 }
 
 function enseñaRadial(coco) {
     let caja_radial = document.getElementById(coco)
     caja_radial.classList.toggle('mostrar_bis')
     caja_radial.classList.toggle('esconder')
- 
+
 }
