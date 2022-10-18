38c38f8901c4abe4a85bce9799025c9ad2219ef2
DnAlfredo <alfredmp6@hotmail.com>
2022-10-19 01:57:48 +0200

    cambio radical, reubico radial y esquema general, más tipo landing

---
+++ progCV.js
@@ -0,0 +1,7 @@
+/*-----------------------RADIAL---------------------------*/
+function enseñaRadial(coco) {
+    let caja_radial = document.getElementById(coco)
+    caja_radial.classList.toggle('mostrar_bis')
+    caja_radial.classList.toggle('esconder')
+  
+  }
