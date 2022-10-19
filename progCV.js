d268dd78060d99a1c6313b9ecbac4a480945bf53
DnAlfredo <alfredmp6@hotmail.com>
2022-10-19 13:51:21 +0200

    Nuevo proyecto

--- progCV.js
+++ progCV.js
@@ -1,7 +1,12 @@
 /*-----------------------RADIAL---------------------------*/
 function enseñaRadial(coco) {
-    let caja_radial = document.getElementById(coco)
-    caja_radial.classList.toggle('mostrar_bis')
-    caja_radial.classList.toggle('esconder')
+    let caja_radial = document.getElementById(coco);
+    caja_radial.classList.toggle('mostrar');
+    caja_radial.classList.toggle('esconder');
   
+  }
+  function abreFicha(idloca) {
+    let divgoster= document.getElementById(idloca);
+    divgoster.classList.toggle('mostrar');
+    divgoster.classList.toggle('esconder')
   }
