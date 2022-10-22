01e6fca1ca673b23698ab892f5c828cd5613e9d3
DnAlfredo <alfredmp6@hotmail.com>
2022-10-22 12:06:47 +0200

    retoco y encuentro dinamismo interesante en el fixed del cntct

--- progCV.js
+++ progCV.js
@@ -1,14 +1,26 @@
 /*-----------------------RADIAL---------------------------*/
 function enseñaRadial(coco) {
-    let caja_radial = document.getElementById(coco);
-    caja_radial.classList.toggle('mostrar');
-    caja_radial.classList.toggle('esconder');
-  
-  }
- 
+  let caja_radial = document.getElementById(coco);
+  caja_radial.classList.toggle('mostrar');
+  caja_radial.classList.toggle('esconder');
 
-  function abreFicha(idloca) {
-    let divgoster= document.getElementById(idloca);
-    divgoster.classList.toggle('mostrar');
-    divgoster.classList.toggle('esconder')
-  }
+}
+
+
+function abreFicha(idloca) {
+  let divgoster = document.getElementById(idloca);
+  divgoster.classList.toggle('mostrar');
+  divgoster.classList.toggle('esconder')
+
+}
+
+
+
+function abreFichacntct(idloca) {
+  let divgoster = document.getElementById(idloca);
+  divgoster.classList.toggle('mostrar_cntct')
+  divgoster.classList.toggle('esconder_cntct')
+  document.getElementById('cont_cntct').classList.toggle('mostrar_cntct')
+}
+/* arriba he tenido que crear una esconder y mostrar especial para cambiar el tamaño haciendo toogle. Hay que coger la id del texto a enseñar mientrras el
+ ccont se queda con esconder del tamaño deseado y al mostrar le decimos que le haga el mostrar al cont también*/
