c9e1a123656fcfa9f67eb7b53ffb40167d6dc57e
DnAlfredo <alfredmp6@hotmail.com>
2022-11-10 11:45:46 +0100

    empiezo a subir ejemplos

--- progCV.js
+++ progCV.js
@@ -24,3 +24,41 @@ function abreFichacntct(idloca) {
 }
 /* arriba he tenido que crear una esconder y mostrar especial para cambiar el tamaño haciendo toogle. Hay que coger la id del texto a enseñar mientrras el
  ccont se queda con esconder del tamaño deseado y al mostrar le decimos que le haga el mostrar al cont también*/
+
+ /*----------------------------------------Zona práctica 2-----------------------------------*/
+ let coleccionfotos = ['./multimedia/DSCN0083.jpg ', './multimedia/DSCN0155.jpg', './multimedia/IMG_0106.jpg','./multimedia/IMG_0147.jpg','./multimedia/IMG_0207.jpg']
+ let posicion = 0
+ let intervalo
+ let interruptor = false
+
+ function muestraImg(id) {
+
+     let img = document.getElementById('img_galeria')
+     img.src = './multimedia/logo_frog.png '
+     document.getElementById('divimg_galeria').style.display = "flex"
+     document.getElementById('botones_mini').style.display = "flex"
+     document.getElementById('boton_inicio').style.display = "none"
+ }
+
+ function cambiaImg(num) {
+     let img = document.getElementById('img_galeria')
+
+     posicion = posicion + num
+
+     if (posicion < 0) {
+         posicion = coleccionfotos.length - 1 //asi podriamos saber cuantos hay dentro del array, sacar la longitud y quitarle el de la cuenta del cero con el menos uno
+     }
+     if (posicion >= coleccionfotos.length) {
+         posicion = 0
+     }
+
+     img.src = coleccionfotos[posicion]
+ }
+
+ function fotoDirecta(pos) {
+     //falta poner filtros para que no tenga errores
+     let img = document.getElementById('img_galeria')
+     img.src = coleccionfotos[pos]
+     posicion = pos //para seguir con la misma caja para el resto
+ }
+
