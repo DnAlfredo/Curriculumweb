76a3f6e55e776da8dd64924664791e07de7e0ac9
DnAlfredo <alfredmp6@hotmail.com>
2022-11-14 13:09:21 +0100

    añado ejemplos en zona de publicación

--- carta digital/jsCarta.js
+++ carta digital/jsCarta.js
@@ -1,6 +1,19 @@
 function abreFichaRte(idloca) {
-    let divgoster = document.getElementById(idloca);
-    divgoster.classList.toggle('mostrar_rte');
-    divgoster.classList.toggle('esconder_rte');
-   
-  }
+  ocultarTodos()
+  let divgoster = document.getElementById(idloca);
+  divgoster.classList.toggle('mostrar_rte');
+
+}
+function ocultarTodos() {
+  mostrados = document.querySelectorAll('.cont_img_plt, .mostrar_rte')
+  if (mostrados) mostrados.forEach((m) => m.classList.remove('mostrar_rte'))
+}
+clickeables = document.querySelectorAll('.cont_img_plt')
+clickeables.forEach((c) => c.addEventListener("click", abreFichaRte, false))
+function abreFichaMenu(idloca) {
+
+  let divgoster = document.getElementById(idloca);
+  divgoster.classList.toggle('mostrar_rte');
+  divgoster.classList.toggle('esconder_rte');
+  
+}
