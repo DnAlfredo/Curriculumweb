c066c8fefd5491c19f4d75d0533913193759de55
DnAlfredo <alfredmp6@hotmail.com>
2022-11-16 11:49:21 +0100

    retoco carte rte

--- carta digital/jsCarta.js
+++ carta digital/jsCarta.js
@@ -1,9 +1,10 @@
+/* anulada por funcion ofrecida en stack que funciona mucho mejor
 function abreFichaRte(idloca) {
   ocultarTodos()
   let divgoster = document.getElementById(idloca); 
   divgoster.classList.toggle('mostrar_rte');
 }
-
+*/
 function ocultarTodos() {
   mostrados = document.querySelectorAll('.cont_img_plt, .mostrar_rte')
   if (mostrados) mostrados.forEach((m) => m.classList.remove('mostrar_rte'))
@@ -20,3 +21,16 @@ function ampliaImg(nom) {
   let divmg=document.getElementById(nom);
   divmg.classList.toggle('img_ampliada');
 }
+function abreFichaRte(el) {
+  let padre = el.parentNode
+  let divhijo = padre.querySelector('.cont_img_plt');
+  document.querySelectorAll('.cont_img_plt').forEach(function(x) {
+    if (x != divhijo) {
+      x.classList.remove('mostrar_rte')
+      x.classList.add('esconder_rte')
+    }
+  })
+  divhijo.classList.toggle('mostrar_rte')
+  divhijo.classList.toggle('esconder_rte')
+}
+
