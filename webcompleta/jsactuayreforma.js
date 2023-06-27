d73267e87b118aede6ec91b4772dad4d8fd7ee44
DnAlfredo <alfredmp6@hotmail.com>
2023-06-27 11:16:58 +0200

    ultima actualizacion

---
+++ webcompleta/jsactuayreforma.js
@@ -0,0 +1,6 @@
+function abreFichaMenu(idloca) {
+    let divgoster = document.getElementById(idloca);
+    divgoster.classList.toggle('mostrar_rte');
+    divgoster.classList.toggle('esconder_rte');
+    
+  }
